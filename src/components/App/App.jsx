a8ad211bfcf3c4f113a8b8c9d6d615db5e633138
Olga Sumaruk <olga_sumaruk@hotmail.com>
2022-11-04 11:26:02 +0200

    update App using hook

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { PageTitle } from 'components/PageTitle/PageTitle.styled';
 import { Container } from './App.styled';
 import { Section } from 'components/Section/Section';
@@ -7,46 +7,47 @@ import { Statistics } from 'components/Statistics/Statistics';
 import { Notification } from 'components/Notification/Notification';
 
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  };
-  
-  handleLeaveFeedback = (item) => {
-    this.setState(prevState => {
-      return ({
-        ...prevState,
-        [item]: prevState[item] + 1,
-      })
-    });
-  };
-  
-  totalFeedback = () => Object.values(this.state).reduce((previousValue, number) => {
-  return previousValue + number;
-}, 0);
+export function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  
-  positivePercentage = () => {
-    return Number(((this.state.good / this.totalFeedback()) * 100).toFixed(0));
+  const options = ['good', 'neutral', 'bad'];
+
+  const handleLeaveFeedback = (item) => {    
+    switch (item) {
+      case 'good':
+        setGood(prev => prev + 1);
+        break;
+      case 'neutral':
+        setNeutral(prev => prev + 1);
+        break;
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+      default:
+        break;
+    }
   }
-  
-  render() {
-    const {state, handleLeaveFeedback, totalFeedback, positivePercentage } = this;
-    const { good, neutral, bad } = state;
-    const options = Object.keys(state);
-    return (<>
+
+  const totalFeedback = () => {
+    return good + neutral + bad;
+  }
+
+  const positivePercentage = () => {     
+    return ((good / totalFeedback()) * 100).toFixed(0);
+  }
+
+  return (<>
     <PageTitle>goit react hw 02 feedback</PageTitle>
-      <Container>        
-        <Section title="Please leave feedback"> 
-          <FeedbackOptions options={options} onLeaveFeedback={handleLeaveFeedback}></FeedbackOptions>
-        </Section> 
-        <Section title="Statistics">
-          {totalFeedback() ? <Statistics good={good} neutral={neutral} bad={bad} total={totalFeedback()} positivePercentage={positivePercentage()} />  : <Notification message="There is no feedback"/>}           
-        </Section>
-      </Container>
-      </>
-    );
-  }
+    <Container>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={options} onLeaveFeedback={handleLeaveFeedback}></FeedbackOptions>
+      </Section>
+      <Section title="Statistics">
+        {totalFeedback() ?
+          <Statistics good={good} neutral={neutral} bad={bad} total={totalFeedback()} positivePercentage={positivePercentage()} /> : <Notification message="There is no feedback" />}
+      </Section>
+    </Container>
+  </>);
 }
